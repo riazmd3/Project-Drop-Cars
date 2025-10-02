8b57f498ed2efe80c42eb2bb82ae2a835311ed28
Pugazheshwar <pugazheshwar411@gmail.com>
2025-10-02 10:31:49 +0530

    Made dev build 2day of leave

--- Vendor App/config/environment.ts
+++ Vendor App/config/environment.ts
@@ -2,7 +2,7 @@
 export const ENV = {
   // Development environment
   development: {
-    API_BASE_URL: 'http://172.20.10.7:8000',
+    API_BASE_URL: 'http://192.168.1.20:8000',
     DEBUG: true,
     LOG_LEVEL: 'debug',
   },
