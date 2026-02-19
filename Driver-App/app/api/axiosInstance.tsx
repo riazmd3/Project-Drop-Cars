a7cfbce5e3bf5287af7d3587275e210a0b04014d
riazmd3 <rockriaz5@gmail.com>
2026-02-19 15:49:14 +0530

    Updayed Script

--- Driver-App/app/api/axiosInstance.tsx
+++ Driver-App/app/api/axiosInstance.tsx
@@ -4,8 +4,8 @@ import * as SecureStore from 'expo-secure-store';
 import { emitSessionExpired } from '@/utils/session';
 
 // For React Native, use machine IP instead of localhost
-const API_BASE_URL = 'http://10.116.186.247:8000/';
-// const API_BASE_URL = 'https://drop-cars-api-207918408785.asia-south2.run.app';
+// const API_BASE_URL = 'http://10.116.186.247:8000/';
+const API_BASE_URL = 'https://drop-cars-api-207918408785.asia-south2.run.app';
 console.log('🔧 API Config:', { baseURL: API_BASE_URL });
 
 const axiosInstance = axios.create({
